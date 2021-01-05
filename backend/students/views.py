f675cdccc7b7e5b4178e00743a42e2a036913104
Darvesh1 <aminjony@gmail.com>
2021-01-05 23:00:23 +0600

    Model to dict

--- backend/students/views.py
+++ backend/students/views.py
@@ -1,6 +1,17 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from .models import Student
+
 def index(request):
+    # students = Student.objects.all()
     students = []
+
+    for student in Student.objects.all():
+        students.append({
+            'name': student.name,
+            'course': student.course,
+            'rating': student.rating,
+        })
+
     return JsonResponse(students, safe=False)
